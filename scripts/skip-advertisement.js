c12a3dc18bedfb4e30b2fcc8581449071df1dd18
Jayant Malik <dev.jayantmalik@gmail.com>
2023-05-17 13:51:11 +0530

    updated details and added ad skip script source code

---
+++ scripts/skip-advertisement.js
@@ -0,0 +1,18 @@
+/**
+ * Extension: scripty
+ * Name: Youtube AD Auto Remove
+ * Run script if: URL -> Contains -> youtube.com
+ * Trigger: Automatically -> On Page Load
+ */
+
+setInterval(function () {
+  //Skip AD Video
+  if (document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text")) {
+    document.querySelector(".ytp-ad-text.ytp-ad-skip-button-text").click();
+  }
+
+  //Close AD Banner
+  if (document.querySelector(".ytp-ad-overlay-close-button")) {
+    document.querySelector(".ytp-ad-overlay-close-button").click();
+  }
+}, 3000);
